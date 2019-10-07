5d06599ddac9fe3226be22ec9fd6c115fb78d974
Adam Goren <amg1090@nyu.edu>
2019-10-07 18:05:30 -0400

    Got fixed jumping to function, began level design

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,10 +8,18 @@ public class PlayerController : MonoBehaviour
 
     float horizontalInput;
 
+    public float jumpVelocity = 2;
+
+    public bool jumpCheck;
+
+    public float fallMultiplier = 2.5f;
+
+    Rigidbody2D playerRB;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -31,5 +39,31 @@ public class PlayerController : MonoBehaviour
         }
 
         transform.position += new Vector3 (horizontalInput, 0f, 0f);
+
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && jumpCheck == true)
+        {
+            playerRB.velocity = Vector2.up * jumpVelocity;
+        }
+
+        if (playerRB.velocity.y < 0)
+        {
+            playerRB.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            jumpCheck = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            jumpCheck = false;
+        }
     }
 }
